732a11e4bcc8958b4cd4582f452bf5f20046aa80
Arianna Asbjornsen <ariannaasbjornsen@wifi-10-43-165-29.wifi.berkeley.edu>
2024-01-30 18:36:42 -0800

    this file is me figuring out pyhton for the first time

--- mydeath.py
+++ mydeath.py
@@ -1,2 +1,3 @@
 print(3.14159*365+18000) #this represents how many times a day I reconsider my life choices
 print("this represents how many times a day I reconsider my life choices.")
+#to run this file I would type in python mydeath.py 
